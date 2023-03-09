50848497fa2920473b3c4739b39b953a2f260adb
raj121276thakre <raj121276thakre@gmail.com>
2023-03-09 22:56:12 +0530

    3] home fragment, map

--- app/src/main/java/com/rajapps/spcoetcollege/ui/home/HomeFragment.java
+++ app/src/main/java/com/rajapps/spcoetcollege/ui/home/HomeFragment.java
@@ -1,5 +1,8 @@
 package com.rajapps.spcoetcollege.ui.home;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +10,37 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.rajapps.spcoetcollege.R;
 
 
 public class HomeFragment extends Fragment {
 
+    private ImageView map;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view= inflater.inflate(R.layout.fragment_home, container, false);
+
+        map = view.findViewById(R.id.map);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMap();
+            }
+        });
+
+        return view;
+    }
+
+    private void openMap() {
+        Uri uri = Uri.parse("geo:0, 0?q=476J+9H5 SEC,ENGINEERING COLLEGE, Someshwar Nagar, Vadgoan Sheri, Pune, Maharashtra 412306");
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        intent.setPackage("com.google.android.apps.maps");
+        startActivity(intent);
     }
 }
