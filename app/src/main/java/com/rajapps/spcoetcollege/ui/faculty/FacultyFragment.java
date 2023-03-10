99e6d3a4dc9c116a6d6a60c812b64d56cdd68b54
raj121276thakre <raj121276thakre@gmail.com>
2023-03-10 22:53:58 +0530

    6] Faculty Fragment

--- app/src/main/java/com/rajapps/spcoetcollege/ui/faculty/FacultyFragment.java
+++ app/src/main/java/com/rajapps/spcoetcollege/ui/faculty/FacultyFragment.java
@@ -2,23 +2,207 @@ package com.rajapps.spcoetcollege.ui.faculty;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.rajapps.spcoetcollege.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class FacultyFragment extends Fragment {
 
+    private RecyclerView csDepartment, mechanicalDepartment, civilDepartment, electDepartment;
+    private LinearLayout csNoData, mechNoData, civilNoData, electNoData  ;
+    private List<TeacherData> list1, list2, list3, list4  ;
+    private DatabaseReference reference, dbRef;
+
+    private TeacherAdapter adapter;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_faculty, container, false);
+        View view = inflater.inflate(R.layout.fragment_faculty, container, false);
+
+
+        csDepartment = view.findViewById(R.id. csDepartment);
+        mechanicalDepartment = view.findViewById(R.id. mechanicalDepartment);
+        civilDepartment = view.findViewById(R.id.civilDepartment);
+        electDepartment = view.findViewById(R.id.electDepartment);
+
+        csNoData = view.findViewById(R.id. csNoData);
+        mechNoData = view.findViewById(R.id. mechNoData);
+        civilNoData= view.findViewById(R.id.civilNoData);
+        electNoData = view.findViewById(R.id.electNoData);
+
+
+        reference= FirebaseDatabase.getInstance().getReference().child("teacher");
+
+        csDepartment();
+        mechanicalDepartment();
+        civilDepartment();
+        electDepartment();
+
+
+      return view;
+
+    }//......................functions.................
+
+
+
+    private void csDepartment() {
+
+        dbRef= reference.child("Computer science");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list1 = new ArrayList<>();
+                if (!dataSnapshot.exists()){
+                    csNoData.setVisibility(View.VISIBLE);
+                    csDepartment.setVisibility(View.GONE);
+                }else {
+                    csNoData.setVisibility(View.GONE);
+                    csDepartment.setVisibility(View.VISIBLE);
+
+                    for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                        TeacherData data = snapshot.getValue(TeacherData.class);
+                        list1.add(data);
+                    }
+                    csDepartment.setHasFixedSize(true);
+                    csDepartment.setLayoutManager(new LinearLayoutManager(getContext()));
+                    adapter =new TeacherAdapter(list1,getContext());
+                    csDepartment.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getContext(), databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
+
+
+    private void mechanicalDepartment() {
+
+        dbRef= reference.child("Mechanical Engineering");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list2 = new ArrayList<>();
+                if (!dataSnapshot.exists()){
+                    mechNoData.setVisibility(View.VISIBLE);
+                    mechanicalDepartment.setVisibility(View.GONE);
+                }else {
+                    mechNoData.setVisibility(View.GONE);
+                    mechanicalDepartment.setVisibility(View.VISIBLE);
+
+                    for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                        TeacherData data = snapshot.getValue(TeacherData.class);
+                        list2.add(data);
+                    }
+                    mechanicalDepartment.setHasFixedSize(true);
+                    mechanicalDepartment.setLayoutManager(new LinearLayoutManager(getContext()));
+                    adapter =new TeacherAdapter(list2,getContext());
+                    mechanicalDepartment.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getContext(), databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+
+    private void civilDepartment() {
+
+        dbRef= reference.child("Civil Engineering");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list3 = new ArrayList<>();
+                if (!dataSnapshot.exists()){
+                    civilNoData.setVisibility(View.VISIBLE);
+                    civilDepartment.setVisibility(View.GONE);
+                }else {
+                    civilNoData.setVisibility(View.GONE);
+                    civilDepartment.setVisibility(View.VISIBLE);
+
+                    for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                        TeacherData data = snapshot.getValue(TeacherData.class);
+                        list3.add(data);
+                    }
+                    civilDepartment.setHasFixedSize(true);
+                    civilDepartment.setLayoutManager(new LinearLayoutManager(getContext()));
+                    adapter =new TeacherAdapter(list3,getContext());
+                    civilDepartment.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getContext(), databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+
+    private void electDepartment() {
+
+        dbRef= reference.child("Electrical Engineering");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list4 = new ArrayList<>();
+                if (!dataSnapshot.exists()){
+                    electNoData.setVisibility(View.VISIBLE);
+                    electDepartment.setVisibility(View.GONE);
+                }else {
+                    electNoData.setVisibility(View.GONE);
+                    electDepartment.setVisibility(View.VISIBLE);
+
+                    for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                        TeacherData data = snapshot.getValue(TeacherData.class);
+                        list4.add(data);
+                    }
+                    electDepartment.setHasFixedSize(true);
+                    electDepartment.setLayoutManager(new LinearLayoutManager(getContext()));
+                    adapter =new TeacherAdapter(list4,getContext());
+                    electDepartment.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getContext(), databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+
+
+
+
 }
